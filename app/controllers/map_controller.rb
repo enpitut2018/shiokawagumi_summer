2b723878a4512abfa82946b20be473e7a93f7f15
kyonezu <yonezu.dev@gmail.com>
2018-07-24 16:53:21 +0900

    adde top page

---
+++ app/controllers/map_controller.rb
@@ -0,0 +1,20 @@
+class MapController < ApplicationController
+	# require 'instagram'
+	# require 'uri'
+	# require 'json'
+
+	def show_pictures
+		# Instagram.configure do |config|
+		# 	config.client_id = "#{Constants::CLIENT_ID}"
+		# 	config.client_secret = "#{Constants::CLIENT_SECRET}"
+		# 	config.access_token = "#{Constants::ACCESS_TOKEN}"
+		# end
+    #
+		# unless Instagram.tag_recent_media("はんにゃらばんばんばん")
+		# 	@medias = ""
+		# else
+		# 	@medias = Instagram.tag_recent_media("はんにゃらばんばんばん")
+		# end
+		# @medias = Instagram.tag_recent_media("涼しい")
+	end
+end
---
+++ config/initializers/constant.rb
@@ -0,0 +1,8 @@
+module Constants
+  # instagram
+  CLIENT_ID = "41f3ac91bc4d41138a5d95ab100dd7fe"
+  CLIENT_SECRET = "0b881572f8d64e99ba1dbd9ca4a4e240"
+  ACCESS_TOKEN = ""
+  REDIRECT_URL = '******'
+  TAG_NAME = '******'
+end
